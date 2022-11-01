5800658afdb7197ffb37b9eb09b5159f2abb788a
Antoine Bartoli <59134956+AntoineBRTL@users.noreply.github.com>
2022-11-01 20:44:41 +0100

    Delete compiler.ps1

--- compiler.ps1
+++
@@ -1,2 +0,0 @@
-g++ ./src/*.cpp -o bin/LostInSound.exe -I include -include src/precompile.hpp -I src -L lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lwinmm -O0;
-.\bin\LostInSound.exe;
